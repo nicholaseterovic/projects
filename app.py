27c27a8e3aa5ef83bc995d2bd9c29bbe2e9204e1
Nicholas Eterovic <nicholas@eterovic.com>
2020-12-28 14:47:03 -0500

    + PivotTable project

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # Open-source Dash packages.
 import dash
 import dash_tabulator as dtb
+import dash_pivottable as dpv
 import dash.exceptions as dxc
 import dash.dependencies as ddp
 import dash_core_components as dcc
@@ -13,7 +14,12 @@ import dash_bootstrap_components as dbc
 import dash_extensions.javascript as djs
 
 # Open-source miscellanous packages.
+import numpy as np
+import pandas as pd
 import typing as tp
+import plotly.data as ptd
+import sklearn.utils as sku
+import sklearn.datasets as skd
 
 # In-house packages.
 import util.system as su
@@ -40,6 +46,7 @@ ns = djs.Namespace('dapp', 'tabulator')
 projects = {
     'home':{'label':'Home', 'icon':'fas fa-home'},
     'cube':{'label':'Rubik\'s Cube', 'icon':'fas fa-cube'},
+    'pivot':{'label':'Pivot Table', 'icon':'fas fa-border-all'},
 }
 
 links = {
@@ -78,15 +85,24 @@ base_figure = {
     },
 }
 
+datasets = {
+    'sklearn':{
+        source:[func for func in skd.__dir__() if func.startswith(source)]
+        for source in ['load', 'fetch', 'make']
+    },
+    'plotly':{
+        'load':[func for func in ptd.__dir__() if not func.startswith('_')],
+    },
+}
+
 app.layout = dhc.Div(style={'position':'relative'}, children=[
     dtc.SideBar(bg_color='#2f4f4f', children=[
-        dtc.SideBarItem(label='PROJECTS'),
+        dhc.Hr(),
         *[
             dtc.SideBarItem(id=f'sbi-{project}', icon=kwargs['icon'], label=kwargs['label'])
             for project, kwargs in projects.items()
         ],
         dhc.Hr(),
-        dtc.SideBarItem(label='LINKS'),
         *[
             dhc.Div(style={'margin-left':'20px'}, children=[
                 dcc.Link(
@@ -217,14 +233,90 @@ app.layout = dhc.Div(style={'position':'relative'}, children=[
                                         'headerSortStartingDir':'desc',
                                     }],
                                     options={
-                                        'placeholder':'No moves made',
+                                        'placeholder':'None',
                                         'layout':'fitDataStretch',
+                                        'height':'100vh',
+                                        'minHeight':'100vh',
                                         'maxHeight':'100vh',
                                         'selectable':'false',
                                     },
                                 ),
                             ]),
                         ]),
+                    ],
+                ),
+                dcc.Tab(
+                    value='pivot',
+                    style={'line-height':'0px'},
+                    children=[
+                        dbc.InputGroup(
+                            size='sm',
+                            children=[
+                                dbc.DropdownMenu(
+                                    label='Load Data',
+                                    color='primary',
+                                    direction='right',
+                                    addon_type='append',
+                                    children=[
+                                        dbc.DropdownMenuItem(
+                                            children='Upload',
+                                            n_clicks=0,
+                                            disabled=True,
+                                        ),
+                                    ] + [
+                                        dbc.DropdownMenu(
+                                            label=library.capitalize(),
+                                            direction='right',
+                                            color='link',
+                                            disabled=(library=='sklearn'),
+                                            children=[
+                                                dbc.DropdownMenu(
+                                                    label=source.capitalize(),
+                                                    direction='right',
+                                                    color='link',
+                                                    children=[
+                                                        dbc.DropdownMenuItem(
+                                                            id=f'pivot-dmi-{library}-{source}-{func}',
+                                                            children=func.split('_', maxsplit=1)[-1],
+                                                            disabled=(source=='fetch'),
+                                                            n_clicks=0,
+                                                        )
+                                                        for func in funcs
+                                                    ],
+                                                )
+                                                for source, funcs in sources.items()
+                                            ],
+                                        )
+                                        for library, sources in datasets.items()
+                                    ],
+                                ),
+                                dbc.Input(
+                                    type='text',
+                                    disabled=True,
+                                ),
+                            ],
+                        ),
+                        dtb.DashTabulator(
+                            id='pivot-tabulator-raw',
+                            data=[],
+                            columns=[{
+                                'title':'',
+                                'field':'none',
+                                'hozAlign':'center',
+                                'headerSort':False,
+                            }],
+                            options={
+                                'placeholder':'None',
+                                'layout':'fitDataStretch',
+                                'layoutColumnsOnNewData':False,
+                                'minHeigh':'50vh',
+                                'maxHeight':'50vh',
+                                'height':'50vh',
+                                'pagination':'local',
+                                'selectable':False,
+                            },
+                        ),
+                        dhc.Div(id='pivot-div-agg', children=[]),
                     ],
                 ),
             ],
@@ -330,6 +422,65 @@ def enable_scramble_button(dim:int, figure:dict) -> bool:
         return True
     return not trigger['value'].get('data', [])
 
+####################################################################################################
+# PIVOT TABLE
+
+@app.callback(
+    [
+        ddp.Output('pivot-tabulator-raw', 'data'),
+        ddp.Output('pivot-tabulator-raw', 'columns'),
+    ],
+    [
+        ddp.Input(f'pivot-dmi-{library}-{source}-{func}', 'n_clicks')
+        for library, sources in datasets.items()
+        for source, funcs in sources.items()
+        for func in funcs
+    ],
+)
+def load_raw_data(*n_clicks:tp.List[str]) -> tp.List[dict]:
+    trigger = dash.callback_context.triggered[0]
+    if not trigger['value']:
+        raise dxc.PreventUpdate
+    
+    comp = trigger['prop_id'].rsplit('.', maxsplit=1)[0]
+    *_, library, source, func = comp.split('-')
+
+    if library=='plotly':
+        data = getattr(ptd, func)()
+    elif library=='sklearn':
+        output = getattr(skd, func)()
+        if isinstance(output, sku.Bunch):
+            
+            Xdat = output.data
+            if len(Xdat.shape)==1:
+                Xdat = Xdat[:, np.newaxis]
+
+            Xcol = output.get('feature_names', range(Xdat.shape[1]))
+
+            X = dict(zip(Xcol, Xdat.T))
+        data = pd.DataFrame(X)
+    else:
+        data = pd.DataFrame()
+
+    records = data.to_dict(orient='records')
+    columns = [
+        {
+            'title':col,
+            'field':col,
+            'hozAlign':'center',
+        }
+        for col in data.columns
+    ]
+    return records, columns
+
+@app.callback(
+    ddp.Output('pivot-div-agg', 'children'),
+    [ddp.Input('pivot-tabulator-raw', 'data')],
+)
+def load_agg_data(records:tp.List[dict]) -> dpv.PivotTable:
+    n = sum(len(col) for col in records[0])
+    return dpv.PivotTable(id=str(pd.Timestamp.now()), data=records, unusedOrientationCutoff=n+1)
+                        
 ####################################################################################################
 # DEPLOY
 
