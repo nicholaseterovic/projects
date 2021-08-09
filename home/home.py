318376b0ace67a7d60d1cf834c71cf0f1e81d780
Nicholas Eterovic <nicholas@eterovic.com>
2021-08-08 23:03:59 -0400

    + Boid to app

--- home/home.py
+++ home/home.py
@@ -24,9 +24,6 @@ app_layout = [
                             # Home
                             ***
 
-                            ### This Page
-                            ***
-
                               Greetings!
                               
                               You've stumbled upon my humble personal website,
