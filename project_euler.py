8ebd2a582998b1b496e1f4bc26c7707960d3376f
Nicholas Eterovic <nicholas@eterovic.com>
2021-10-03 18:59:00 -0400

    + Solutions 15, 19, 20

--- project_euler.py
+++ project_euler.py
@@ -3,7 +3,6 @@
 
 # Open-source packages.
 import math
-from operator import pos
 import typing as tp
 import functools as ft
 import itertools as it
@@ -345,5 +344,84 @@ def solution_014(max_num:int=1e6) -> int:
     nums = range(1, int(max_num))
     collatz_tree = CollatzTree()
     return max(nums, key=collatz_tree.get_length)
+
+####################################################################################################
+
+def factorial(n:int) -> int:
+    factorial = 1
+    for i in range(2, n+1):
+        factorial *= i
+    return factorial
+
+def get_num_permutations(container:tp.Container) -> int:
+    n = len(container)
+    R = [len(list(group)) for key, group in it.groupby(sorted(container))]
+    num_permutations = factorial(n)//prod(map(factorial, R))
+    return num_permutations
+    
+def solution_015(grid_length:int=20) -> int:
+    moves = grid_length*['DOWN', 'RIGHT']
+    return get_num_permutations(moves)
+
+####################################################################################################
+
+def is_leap_year(YYYY:int) -> bool:
+    if YYYY % 100 == 0:
+        if YYYY % 400 == 0:
+            return True
+        return False
+    if YYYY % 4 == 0:
+        return True
+    return False
+
+def get_num_days_in_month(YYYY:int, MM:int) -> int:
+    if MM==2:
+        if is_leap_year(YYYY):
+            return 29
+        return 28
+    if MM in [4, 6, 9, 11]:
+        return 30
+    return 31
+
+def parse_date(YYYYMMDD:int) -> tp.Tuple[int, int, int]:
+    DD = YYYYMMDD % 100
+    MM = ((YYYYMMDD-DD) % 10000) // 100
+    YYYY = (YYYYMMDD-100*MM-DD) // 10000
+    return (YYYY, MM, DD)
+    
+def get_dates_between(sYYYYMMDD:int, eYYYYMMDD:int) -> tp.List[int]:
+    sYYYY, sMM, sDD = parse_date(YYYYMMDD=sYYYYMMDD)
+    eYYYY, eMM, eDD = parse_date(YYYYMMDD=eYYYYMMDD)
+    return [
+        10000*YYYY+100*MM+DD
+        for YYYY in range(
+            sYYYY,
+            eYYYY+1,
+        )
+        for MM in range(
+            sMM if YYYY==sYYYY else 1,
+            (eMM if YYYY==eYYYY else 12)+1,
+        )
+        for DD in range(
+            sDD if YYYY==sYYYY and MM==sMM else 1,
+            (eDD if YYYY==eYYYY and MM==eMM else get_num_days_in_month(YYYY, MM))+1,
+        )
+    ]
+
+def solution_019(sYYYYMMDD:int=19010101, eYYYYMMDD:int=20001231, day_of_week:int=7) -> int:
+    date_mon = 19000101
+    dates = get_dates_between(min(date_mon, sYYYYMMDD), max(date_mon, eYYYYMMDD))
+    i_mon = dates.index(date_mon)
+    i_dow = i_mon+day_of_week-1
+    return len([
+        date
+        for date in dates[i_dow%7::7]
+        if sYYYYMMDD<=date<=eYYYYMMDD and parse_date(date)[2]==1
+    ])
+
+####################################################################################################
+
+def solution_020(n:int=100) -> int:
+    return sum(int(digit) for digit in str(factorial(n=n)))
 
 ####################################################################################################
