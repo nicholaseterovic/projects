41547c5267bb8a1de3f33e4fa2daf133a35d6729
Nicholas Eterovic <nicholas@eterovic.com>
2021-11-13 09:17:10 -0500

    get_intersection to its own funcs

--- utils/sets.py
+++ utils/sets.py
@@ -3,18 +3,15 @@
 
 # Open-source packages.
 import typing as tp
-import functools as ft
 import itertools as it
+import functools as ft
 
 ####################################################################################################
 
-def get_intersection(sets:tp.List[set]) -> set:
-    """
-    > Intersect <sets>.
-    """
+def get_intersection_sets(sets:tp.List[str]) -> set:
     return ft.reduce(set.intersection, sets, set())
 
-def get_set_union_agg(sets:tp.List[set], agg:tp.Callable=len) -> int:
+def get_agg_union_sets(sets:tp.List[set], agg:tp.Callable=len) -> int:
     """
     > Aggregate <sets> using the inclusion-exclusion principle.
     """
@@ -24,7 +21,7 @@ def get_set_union_agg(sets:tp.List[set], agg:tp.Callable=len) -> int:
     for i in I:
         sign = 1 if i%2==1 else -1
         subsets = it.combinations(sets, i)
-        intersections = map(get_intersection, subsets)
+        intersections = map(get_intersection_sets, subsets)
         counts = map(agg, intersections)
         count += sign*sum(counts, 0)
     return count
